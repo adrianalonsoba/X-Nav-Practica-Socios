5637fe74dd33fa43a3440cc9ab7b5f5b775caf9d
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-03-16 19:06:03 +0100

    añadidos jsons de prueba

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 jQuery(document).ready(function() {
 
 	$( "#tabs" ).tabs();
+
+	$( "#timeline" ).click(function(){
+	    $.getJSON("jsons/timeline.json", function(data) {
+	        alert(data[0].author);
+	    });
+	});
+
+	$( "#sent_messages" ).click(function(){
+	    $.getJSON("jsons/myline.json", function(data2) {
+	        alert(data2[0].author);
+	    });
+	});
+
+
 	
 });
 
