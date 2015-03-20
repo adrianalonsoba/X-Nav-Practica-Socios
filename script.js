220d2a022f845c66ca9f88577636d2e24efd3adf
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-03-20 11:25:44 +0100

    cambiado de dialog a accordion

--- script.js
+++ script.js
@@ -3,34 +3,23 @@ jQuery(document).ready(function() {
 
 	$( "#tabs" ).tabs();
 
-	$( "#dialog" ).dialog();
-
 	$( "#timeline" ).click(function(){
 	    $.getJSON("jsons/timeline.json", function(data) {
 	        for (var i = 0; i < data.length; i++) {
-	        	$('#tabs-1').append('<div class="msg">'+
-	        	"<img src="+data[i].avatar+" width='40' height='60'>"+
-	        	'<strong>'+data[i].author+'</strong>'+
-        		//':'+data[i].title+
-        		"<div id='dialog' title='lalaa'>"+
-        		"<p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p></div>"+
-	        	'</div>');
+	        	 $( '#tabs-1' ).append("<div>" + '<img src='+ data[i].avatar + " width='40' height='60'>" + "  " + data[i].author + ":" + data[i].title + "<div>");
+	        	 $( '#tabs-1' ).append("<div>" + data[i].body + "<li>" + data[i].date + "</li>" + "</div>");
 	        };
-	        
+	        $('#tabs-1').accordion({collapsible: true, active:false, heightStyle: "content"}); 
 	    });
 	});
 
 	$( "#sent_messages" ).click(function(){
 	    $.getJSON("jsons/myline.json", function(data) {
 	        for (var i = 0; i < data.length; i++) {
-	        	$('#tabs-2').append('<div class="msg">'+
-	        	"<img src="+data[i].avatar+" width='60' height='60'>"+
-	        	'<strong>'+data[i].author+'</strong>'+
-        		//':'+data[i].title+
-        		"<div id='dialog' title='lalaa'>"+
-        		"<p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p></div>"+
-	        	'</div>');
+	        	 $( '#tabs-2' ).append("<div>" + '<img src='+ data[i].avatar + " width='60' height='60'>" + "  " + data[i].author + ":" + data[i].title + "<div>");
+	        	 $( '#tabs-2' ).append("<div>" + data[i].body + "<li>" + data[i].date + "</li>" + "</div>");
 	        };
+	        $('#tabs-2').accordion({collapsible: true, active:false, heightStyle: "content"}); 
 	    });
 	});
 	
